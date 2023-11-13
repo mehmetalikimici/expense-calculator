49f708e03e960a123394738112500c696f626ff2
mehmetalikimici <malikimici@gmail.com>
2023-11-13 14:37:53 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,114 @@
+const harcamaInput = document.querySelector("#harcama");
+//console.log(harcamaInput)
+const fiyatInput = document.querySelector("#fiyat");
+//console.log(fiyatInput)
+const formBtn = document.querySelector(".ekle-btn");
+//console.log(formBtn)
+const list = document.querySelector(".list");
+//console.log(list)
+const totalInfo = document.querySelector("#total-info");
+//console.log(totalInfo)
+const nameInput = document.getElementById("name-input");
+//console.log(nameInput);
+const statusCheck = document.getElementById("status-input");
+//console.log(statusCheck)
+const selectFilter = document.getElementById("filter-select");
+//console.log(selectFilter)
+const userName = localStorage.getItem("name");
+
+nameInput.value = userName;
+
+nameInput.addEventListener("change", (e) => {
+  //console.log(e.target.value)
+  localStorage.setItem("name", e.target.value);
+});
+
+formBtn.addEventListener("click", addExpense);
+list.addEventListener("click", handleClick);
+selectFilter.addEventListener("change", handleFilter);
+
+let toplam = 0;
+
+function updateToplam(fiyatBilgisi) {
+  //console.log(fiyatBilgisi);
+  toplam += Number(fiyatBilgisi);
+  totalInfo.innerText = toplam;
+}
+
+function addExpense(e) {
+  e.preventDefault();
+  //console.log("addExpense");
+  //console.log(harcamaInput)
+
+  if (!harcamaInput.value || !fiyatInput.value) {
+    alert("Tüm boş alanları doldurun!");
+  } else {
+    const harcamaDiv = document.createElement("div");
+    harcamaDiv.classList.add("expense");
+    if (statusCheck.checked) {
+      //console.log(statusCheck.checked);
+      harcamaDiv.classList.add("paid");
+    }
+    //console.log(statusCheck.checked)
+    harcamaDiv.innerHTML = `
+          <h2>${harcamaInput.value}</h2>
+          <h2 id="value">${fiyatInput.value}</h2>
+          <div class="buttons">
+            <img id="payment" src="./pay.png" alt="" />
+            <img id="remove" src="./remove.png" alt=""/>
+          </div>
+    `;
+    list.appendChild(harcamaDiv);
+    //console.log(harcamaDiv);
+    updateToplam(fiyatInput.value);
+  }
+  harcamaInput.value = "";
+  fiyatInput.value = "";
+}
+
+function handleClick(e) {
+  //console.log(handleClick)
+  //console.log(e.target)
+  let tiklanilanEleman = e.target;
+  if (tiklanilanEleman.id === "remove") {
+    //console.log(tiklanilanEleman.parentElement.parentElement)
+    const kapsayiciElement = tiklanilanEleman.parentElement.parentElement;
+    //console.log(kapsayiciElement)
+    const deletedPrice = kapsayiciElement.querySelector("#value").innerText;
+    //console.log(deletedPrice)
+    updateToplam(-Number(deletedPrice));
+    kapsayiciElement.remove();
+  }
+}
+
+function handleFilter(e) {
+  //console.log("filtre fonksiyonu");
+  const harcamaKartlari = list.childNodes;
+  //console.log(harcamaKartlari)
+  const filterValue = e.target.value;
+  //console.log(filterValue)
+  harcamaKartlari.forEach((harcamaKarti) => {
+    //console.log(harcamaKarti)
+    switch (filterValue) {
+      case "all":
+        harcamaKarti.style.display = "flex";
+        break;
+
+      case "paid":
+        if (!harcamaKarti.classList.contains("paid")) {
+          harcamaKarti.style.display = "none";
+        } else {
+          harcamaKarti.style.display = "flex";
+        }
+        break;
+
+      case "not-paid":
+        if (harcamaKarti.classList.contains("paid")) {
+          harcamaKarti.style.display = "none";
+        } else {
+          harcamaKarti.style.display = "flex";
+        }
+        break;
+    }
+  });
+}
